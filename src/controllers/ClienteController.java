9c7cb99b923dc77dff7f63b6fd43e536b92f95cd
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2023-05-30 14:41:29 -0300

    Desenvolvimento dos repositótios e controladores

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.List;
 import java.util.Scanner;
 
 import entities.Cliente;
@@ -37,6 +38,87 @@ public class ClienteController {
 		}
 				
 		scanner.close();
+	}
+	
+	public void atualizarCliente() {
+		
+		System.out.println("\n*** ATUALIZAÇÃO DE CLIENTE ***\n");
+		Scanner scanner = new Scanner(System.in);
+		
+		Cliente cliente =  new Cliente();
+		
+		System.out.print("ID DO CLIENTE.......: ");
+		cliente.setIdCliente(Integer.parseInt(scanner.nextLine()));
+		
+		System.out.print("NOME DO CLIENTE.......: ");
+		cliente.setNome(scanner.nextLine());
+		
+		System.out.print("E-MAIL DO CLIENTE.....: ");
+		cliente.setEmail(scanner.nextLine());
+		
+		System.out.print("TELEFONE DO CLIENTE...: ");
+		cliente.setTelefone(scanner.nextLine());
+		
+		System.out.print("CPF DO CLIENTE........: ");
+		cliente.setCpf(scanner.nextLine());
+		
+		ClienteRepository clienteRepository = new ClienteRepository();
+		
+		
+		try {
+			clienteRepository.update(cliente);
+			System.out.println("\nCLIENTE ATUALIZADO COM SUCESSO!");
+		} catch (Exception e) {
+			System.out.println("\nNão foi possível atualizar cliente!");
+			e.printStackTrace(); // imprimir log do erro
+		}
+				
+		scanner.close();
+	}
+	
+	public void excluirCliente() {
+		
+		System.out.println("\n*** DELETAR CLIENTE ***\n");
+		Scanner scanner = new Scanner(System.in);
+		
+		Cliente cliente =  new Cliente();
+		
+		System.out.print("ID DO CLIENTE.......: ");
+		cliente.setIdCliente(Integer.parseInt(scanner.nextLine()));
+		
+		ClienteRepository clienteRepository = new ClienteRepository();
+		
+		try {
+			clienteRepository.delete(cliente);
+			System.out.println("\nCLIENTE DELETADO COM SUCESSO!");
+		} catch (Exception e) {
+			System.out.println("\nNão foi possível deletar cliente!");
+			e.printStackTrace(); // imprimir log do erro
+		}
+				
+		scanner.close();
+	}
+	
+	public void consultarClientes() {
+		System.out.println("\n*** CONSULTA DE CLIENTES ***\n");
+		
+		ClienteRepository clienteRepository = new ClienteRepository();
+		
+		try {
+				List<Cliente> lista = clienteRepository.findAll();
+				
+				for(Cliente cliente : lista) { //foreach
+					System.out.println("ID DO CLIENTE..........: " + cliente.getIdCliente());
+					System.out.println("NOME DO CLIENTE........: " + cliente.getNome());
+					System.out.println("E-MAIL DO CLIENTE......: " + cliente.getEmail());
+					System.out.println("TELEFONE DO CLIENTE....: " + cliente.getTelefone());
+					System.out.println("CPF DO CLIENTE.........: " + cliente.getCpf());
+				}
+				
+		} catch(Exception e) {
+			System.out.println("\nFALHA AO CONSULTAR CLIENTES");
+			e.printStackTrace();// imprimir log de erro
+		}
 	}
 
 }
--- src/repositories/ClienteRepository.java
+++ src/repositories/ClienteRepository.java
@@ -3,6 +3,9 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import entities.Cliente;
 import factories.ConnectionFactory;
@@ -29,19 +32,65 @@ public class ClienteRepository {
 		
 	}
 	
+	// método para receber e atualizar um cliente em banco de dados
 	public void update(Cliente cliente) throws Exception {
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.getConnection();
 		
-		PreparedStatement statement = connection.prepareStatement("UPDATE TABLE CLIENTE (nome, email, telefone, cpf) VALUES(?,?,?,?) WHERE idcliente = ?");
+		// escrevendo um comando em linguagem SQL para atualizar um cliente no banco de dados 
+		PreparedStatement statement = connection.prepareStatement("UPDATE CLIENTE SET nome=?, email=? , telefone=?, cpf=? WHERE idcliente=?");
 		statement.setString(1, cliente.getNome());
 		statement.setString(2, cliente.getEmail());
 		statement.setString(3, cliente.getTelefone());
 		statement.setString(4, cliente.getCpf());
+		statement.setInt(5, cliente.getIdCliente());
 		statement.execute();
 		
 	
 		connection.close();
+	}
+	
+	// método para receber e excluir um cliente no banco de dados
+	public void delete(Cliente cliente) throws Exception {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("DELETE FROM CLIENTE WHERE idcliente=? ORDER BY idcliente");
+		statement.setInt(1, cliente.getIdCliente());
+		statement.execute();
+		
+		connection.close();
+	}
+	
+	// método para consultar todos os clientes cadastrados no banco de dados
+	public List<Cliente> findAll() throws Exception {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM CLIENTE");
+		ResultSet resultSet = statement.executeQuery();
+		
+		// declarando listagem de clientes
+		List<Cliente> lista = new ArrayList<Cliente>();
+		
+		// percorrendo cada registro obtido do banco de dados
+		while(resultSet.next()) {
+			
+			Cliente cliente = new Cliente();
+			
+			cliente.setIdCliente(resultSet.getInt("idcliente"));
+			cliente.setNome(resultSet.getString("nome"));
+			cliente.setEmail(resultSet.getString("email"));
+			cliente.setTelefone(resultSet.getString("telefone"));
+			cliente.setCpf(resultSet.getString("cpf"));
+			
+			lista.add(cliente);
+		}
+		
+		connection.close();
+		return lista;
 	}
 }
